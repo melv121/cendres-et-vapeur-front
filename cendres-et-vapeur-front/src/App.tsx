1dda1d5e1e9d9a57a56f2271da200b0e993ffb78
melv121 <melvinpradier@gmail.com>
2026-02-04 14:04:42 +0100

    creation fichier

--- cendres-et-vapeur-front/src/App.tsx
+++ cendres-et-vapeur-front/src/App.tsx
@@ -1,32 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from './assets/logo.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={logo} className="logo" alt="Cendres et Vapeur logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Cendres et vapeurs </h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        <code>site en cours </code> 
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Se site est super cool !
       </p>
     </>
   )
