660bfb396cf3ff249afdc58f8e16df0743543b7f
Math-Pav <mathias.pavie@gmail.com>
2026-02-11 15:52:42 +0100

    feat:0.0.1 - Correction de css

--- cendres-et-vapeur-front/src/pages/admin/AdminUsersPage.tsx
+++ cendres-et-vapeur-front/src/pages/admin/AdminUsersPage.tsx
@@ -1,11 +1,5 @@
 import AdminUsers from "../../components/admin/AdminUsers";
 
 export default function AdminUsersPage() {
-  return (
-    <section className="adminPage">
-      <h1 className="adminPageTitle">Utilisateurs</h1>
-
-      <AdminUsers />
-    </section>
-  );
+  return <AdminUsers />;
 }
